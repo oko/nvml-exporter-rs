eaf98bf1b08ff5ae6bf7b98819708e4f1e5d0270
Jacob Okamoto <oko@oko.io>
2023-06-28 03:13:29 -0500

    convert to universal service binary for windows

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
 extern crate nvml_exporter as nvml_exporter;
 extern crate nvml_wrapper as nvml;
 
-use futures::future::join_all;
-use log::trace;
+use universal_service::universal_service_main;
 
-async fn server(args: Vec<String>) {
-    let (binds, senders, options) = nvml_exporter::server_setup(args);
-    let server = tokio::spawn(async move {
-        nvml_exporter::serve(binds, options).await;
-    });
-
-    let ctrl_c = tokio::spawn(async move {
-        trace!("spawning ctrl-c handler");
-        // Wait for the CTRL+C signal
-        tokio::signal::ctrl_c().await.expect("failed to install CTRL+C signal handler");
-        trace!("received ctrl-c signal");
-        senders.into_iter().for_each(|s| {
-            let _ = s.send(());
-        });
-    });
-
-    join_all([server, ctrl_c]).await;
-}
-
-#[tokio::main]
-async fn main() {
-    server(std::env::args_os().into_iter().map(|s| s.into_string().unwrap()).collect()).await;
+fn main() -> anyhow::Result<()> {
+    universal_service_main(
+        "nvml-exporter".to_owned(),
+        Box::new(|shutdown_rx: std::sync::mpsc::Receiver<()>, args: Vec<String>, _start_parameters: Option<Vec<String>>| -> anyhow::Result<()> {
+            let rt = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
+            let handle = rt.handle().clone();
+            let (binds, senders, options) = nvml_exporter::server_setup(args);
+            std::thread::spawn(move || {
+                handle.block_on(async move {
+                    let _ = shutdown_rx.recv();
+                    senders.into_iter().for_each(|s| {
+                        let _ = s.send(());
+                    });
+                });
+            });
+            rt.block_on(nvml_exporter::serve(binds, options));
+            Ok(())
+        }),
+    )?;
+    Ok(())
 }
